a889c97a50a01c3a8dbdd072c27de7a6008cae2e
Yatish Kelkar <yatish1606@gmail.com>
2021-04-24 16:51:49 +0530

    Added some styles

--- src/Accordian.tsx
+++ src/Accordian.tsx
@@ -1,4 +1,5 @@
 import React , { useState } from 'react'
+import './Accordion.css'
 
 
 const Accordion : React.FC<{
@@ -18,25 +19,99 @@ const Accordion : React.FC<{
     title = title.toString()
     content = content.toString()
 
+
+    interface style {
+        container : Object,
+        titleContainer: Object,
+        title: Object,
+        content: Object,
+        icon: Object,
+        contentContainer: Object
+    }
+    
+    const styleObject : style = {
+        
+        container : {
+            padding: 20,
+            display: 'flex',
+            flexDirection: 'column',
+            width: wrap ? 'fit-content' : '100%'
+        },
+    
+        titleContainer : {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+        },
+    
+        title : {
+            color: '#22242E',
+            fontSize: 20,
+            letterSpacing: 0.3,
+            textAlign: 'left',
+            flexGrow: 1
+        },
+
+        content : {
+            color: '#636672',
+            fontSize: 15,
+            lineHeight: 22,
+            textAlign: 'justify',
+            width: '100%'
+        },
+
+        icon : {
+            width: 20,
+            height: 20,
+            stroke: '#22242E',
+            strokeWidth: 4,
+        },
+    
+        contentContainer : {
+    
+        },
+
+    }
+
+
     return (
+
         <div
             className="react-accordion-collapsible-container"
-            style = {{
-
-            }}
+            style = {styleObject.container}
         >
             
-            <div className="react-accordion-collapsible-title-conainer">
-                {title}
+            <div className="react-accordion-collapsible-title-container" style={styleObject.titleContainer}>
+                
+                <p className="react-accordion-collapsible-title" style={styleObject.title}>{title}</p>
+                
+                <React.Fragment>
+                {
+                    isOpen ? 
+                    
+                    // Minus icon (accordion open)
+                    <svg className="icon" viewBox="0 0 24 24" width={20} height={20} style={styleObject.icon}>
+                        <path d="M 2,12 L 22,12 M 12,2 L 12,22" />
+                    </svg>
+
+                    : 
+                    
+                    // Plus icon (accordion closed)
+                    <svg className="icon" viewBox="0 0 24 24" width={20} height={20} style={styleObject.icon}>
+                        <path d="M 2,12 L 22,12" />
+                    </svg>
+
+                }
+                </React.Fragment>
+
             </div>
 
             <div 
-                className="react-accordion-collapsible-content-conainer"
-                style={{
-
-                }}
+                className="react-accordion-collapsible-content-container"
+                style={styleObject.contentContainer}
             >
-                {content} {wrap.toString()} {defaultOpen.toString()}
+                <p className="content" style={styleObject.content}>{content}</p>
             </div>
 
         </div>
@@ -44,6 +119,11 @@ const Accordion : React.FC<{
 }
 
 export default Accordion
+
+
+
+
+
 
 /*
 
